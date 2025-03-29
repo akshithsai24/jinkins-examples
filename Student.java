19c1e2a6196033a7c6806c67c5d6b897dd5dccf6
akshithsai24 <245122749019@mvsrec.edu.in>
2025-03-29 14:39:04 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,15 +1,10 @@
-import java.util.Scanner;
 class Student
 {
 	public static void main(String args[])
 	{
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter your Name : ");
-		String name = sc.nextLine();
-		System.out.println("Enter your Roll Number : ");
-		String roll = sc.nextLine();
-		System.out.println("Enter your Branch : ");
-		String brach = sc.nextLine();
+		String name = args[0]
+		String roll = args[1]
+		String brach = args[2]
 		
 		System.out.println("Your Have Entered : ");
 		System.out.println("Name : "+name);
