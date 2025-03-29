e43ef34727e17dffe400d3cb0242d9e7509ef14d
akshithsai24 <245122749019@mvsrec.edu.in>
2025-03-29 15:20:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello World From Python")
+import sys
+n1 = int(sys.argv[1])
+n2 = int(sys.argv[2])
+
+print("Addition : ",n1+n2)
+print("Sub : ",n1-n2)
+print("Mul : ",n1*n2)
+print("Div : ",n1/n2)
